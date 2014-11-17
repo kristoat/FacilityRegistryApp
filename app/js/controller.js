e68f2374412e6c5d5f6bdb3613b2ae2c09411f0d
Quist <joakiml@gmail.com>
2014-11-17 15:46:27 +0100

    dummy modal

--- app/js/controller.js
+++ app/js/controller.js
@@ -18,6 +18,7 @@
                apiService.getFacility(facility.id).get(function(result){
                    setActiveFacility(result.name, "/#/orgunits", facility.id);
                    $scope.facilities = result.children;
+                   console.log($scope.facilities);
                });
             };
 
@@ -42,6 +43,11 @@
                 }
             };
 
+            $scope.showDetails = function(facility){
+                console.log(facility);
+                $scope.selectedFacility = facility;
+            };
+
             function setActiveFacility(text, href, id){
                 for(var i = 0; i < $scope.navItems.length; i++){
                     $scope.navItems[i].active = false;
@@ -64,26 +70,9 @@
         }
     ]);
 
-    controllers.controller('listViewController', ['$scope', 'apiService',
+    controllers.controller('orgunitController', ['$scope', 'apiService',
         function ($scope, apiService) {
-
-            $scope.isHierarchy = true;
-
-            $scope.showHierarchy = function () {
-                $scope.isHierarchy = true;
-            };
-
-            $scope.hideHierarchy = function () {
-                $scope.isHierarchy = false;
-            };
-
-            $scope.chooseFacility = function (facility) {
-                $scope.chosenFacility = facility;
-
-                apiService.getFacilitiesWithParent(facility).query(function (result) {
-                    $scope.topFacilities = result;
-                });
-            };
+            console.log($scope.selectedFacility );
         }
     ]);
 
